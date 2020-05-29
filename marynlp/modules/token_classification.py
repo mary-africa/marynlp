fd76b17fd8fe367c20d3ab4f0f69638aa392e4f3
Kevin James <kevin.al.james@gmail.com>
2020-05-29 20:44:29 +0300

    modified: `TokenClassifier` to support gcp-access

--- marynlp/modules/token_classification.py
+++ marynlp/modules/token_classification.py
@@ -1,81 +1,54 @@
 from pathlib import Path
 from flair.data import Sentence
-from flair.models import SequenceTagger
-from marynlp.data.transformers.specialized import (DataTextTransformer,
-                                                   SwahiliTextTransformer,
-                                                   TextNormalizer,
-                                                   FlairSupportTransformer)
+from flair.models import SequenceTagger as FlairSequenceTagger
 
-DEFAULT_TEXT_TRANSFORMERS_LIST = [SwahiliTextTransformer, TextNormalizer, FlairSupportTransformer]
+from marynlp.data.transformers import DataTextTransformer
+from .module import Module, pretrained_models_class
+from marynlp import SWAHILI_LANGUAGE_CODE
 
 
-class TokenClassifier(object):
-    KNOWN_MODEL_SOURCES = {
-        'sw-ner-base': ('resources/taggers/ner-general-base-model/', 'label')
-    }
-
-    DATA_TEXT_TRANSFORMERS = {
-        'sw-ner-base': DEFAULT_TEXT_TRANSFORMERS_LIST
-    }
+class TokenClassifier(Module):
+    @property
+    def pretrained_models(self):
+        return pretrained_models_class['taggers']
 
     def __init__(self,
-                 tagger: SequenceTagger,
+                 tagger: FlairSequenceTagger,
                  token_label_type: str,
-                 stacked_transformers: list = None):
+                 transformers: DataTextTransformer = None):
 
         self.tagger = tagger
         self.token_label_type = token_label_type
-        self._st_transforms = stacked_transformers
 
-    def transform(self, text: str):
-        if self._st_transforms is None:
-            return text
-
-        assert isinstance(self._st_transforms, list), 'Transforms must be a list'
-
-        for ix, trnsf in enumerate(self._st_transforms):
-            assert isinstance(trnsf,
-                              DataTextTransformer), 'Expected object at index {} to be a {}, instead received a {}' \
-                .format(ix, DataTextTransformer.__name__, type(trnsf).__name__)
-
-            text = trnsf(text)
-
-        return text
+        if transformers:
+            self.transform = transformers
 
     def classify(self, text: str):
-        sentence = Sentence(self.transform(text), language_code='sw')
+        sentence = Sentence(self.transform(text), language_code=SWAHILI_LANGUAGE_CODE)
         self.tagger.predict(sentence)
 
         return sentence
 
     @classmethod
-    def from_pretrained(cls, src: str, token_label_type: str = None):
-        model_src = src.lower()
-        if model_src in cls.KNOWN_MODEL_SOURCES:
-            # might want to download the model first
-            # and store it in this path
-            actual_path, label_type = cls.KNOWN_MODEL_SOURCES[model_src]
+    def from_pretrained(cls, src: str, credentials_json_path: str = None, **clf_kwargs):
+        src = src.lower()
+        model_option = 'best-model.pt'
 
-            tagger = SequenceTagger.load('{}/best-model.pt'.format(actual_path))
+        if src in cls.pretrained_models:
+            # check if the credentials key exists
+            assert credentials_json_path, 'If using pre-trained model, you need to include the credentials key file'
+            assert Path(credentials_json_path).exists(), "Credentials key file doesn't exist"
 
-            # load the contents
-            if model_src in cls.DATA_TEXT_TRANSFORMERS:
-                _trans_class_list = cls.DATA_TEXT_TRANSFORMERS[model_src]
-                trns_stack = [_cls() for _cls in _trans_class_list]
+            model_dir_path = cls._get_pretrained_model_path(src, credentials_json_path)
+        else:
+            model_dir_path = src
 
-                # load with text transformers
-                return cls(tagger, label_type, trns_stack)
+        # use model path
+        model_dir_path = Path(model_dir_path)
 
-            # load without text transformers
-            return cls(tagger, label_type)
+        assert model_dir_path.exists(), 'Model path doesn\'t exist'
 
-            # checks if the path exists
-        assert Path(src).exists(), 'The model path doesn\'t exist'
+        # setting the contents to load the data
+        model_full_path = model_dir_path.joinpath(model_option)
 
-        # otherwise, require token_label_type
-        assert token_label_type is not None, \
-            'If \'{}\' is not from an established source, you must indicate the \'{}\''.format(
-                'src', 'token_label_type'
-            )
-
-        return cls(SequenceTagger.load(src), token_label_type)
+        return cls(FlairSequenceTagger.load(model_full_path), **clf_kwargs)
