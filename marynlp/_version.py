58f22a410b33db50ab77024a86c323cfeb7e7c0a
Kevin James <kevin.al.james@gmail.com>
2020-09-25 13:15:25 +0300

    changed version of package

--- marynlp/_version.py
+++ marynlp/_version.py
@@ -1,9 +1,9 @@
 # Keeping track of the Version used in building the application
 
 _MAJOR = 0
-_MINOR = 4
+_MINOR = 0
 _BUILD = 51
 
-_STATE = 'pre-beta'
+_STATE = 'alpha'
 
 VERSION = f'{_MAJOR}.{_MINOR}.{_BUILD}-{_STATE}'
