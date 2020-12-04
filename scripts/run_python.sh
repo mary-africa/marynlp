5cb3400d5915f22e3a9f43b86e962c2cc1fe5226
iam-kevin <kevin.al.james@gmail.com>
2020-12-04 13:37:36 +0300

    ci: added line to install the package

--- scripts/run_python.sh
+++ scripts/run_python.sh
@@ -10,5 +10,8 @@ python -m pip install -r requirements.txt
 # installing development / testing dependencies
 python -m pip install -r requirements-dev.txt
 
+# installing the package
+python setup.py install
+
 # execute the test file
 $SHELL scripts/run_tests.sh
