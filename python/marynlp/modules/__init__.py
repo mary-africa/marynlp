03c4f12644cf5c8ecb32794626e3e0004c7afecf
iam-kevin <kevin.al.james@gmail.com>
2021-03-15 14:39:26 +0300

    replaced with different name

--- python/marynlp/modules/__init__.py
+++ python/marynlp/modules/__init__.py
@@ -7,7 +7,7 @@ TODO: Make sure the code is NOT case INsensitive
 
 # Embeddings related models
 embeddings_gcp = {
-    'sw-clean-fasttext': 'models/embeddings/sw-clean-ft-100-minn2_maxn5_e50_lr0.1.zip',    
+    'sw-clean-fasttext': 'models/embeddings/sw-clean-ft-dim100-minn2-maxn5-epoch50-lr0.1.zip',    
 }
 
 # Language models
